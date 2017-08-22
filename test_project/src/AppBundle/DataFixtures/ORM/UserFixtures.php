2567ab7b3b2d08bd00f21d31f06910c39497ad23
Praxitelis Kourtelos <pkourtellos@pulsion.co.uk>
2017-08-22 12:28:14 +0100

    Changes to bundle imports, created user entity, enabled authentication from database

---
+++ test_project/src/AppBundle/DataFixtures/ORM/UserFixtures.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pkourtellos
+ * Date: 22/08/2017
+ * Time: 12:08
+ */
+
+namespace AppBundle\DataFixtures\ORM;
+
+use AppBundle\Entity\User;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+
+class UserFixtures extends AbstractFixture implements OrderedFixtureInterface,ContainerAwareInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+    /**
+     * {@inheritDoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+    public function load( ObjectManager $manager )
+    {
+        $factory = $this->container->get('security.encoder_factory');
+
+
+        $user1 = new User();
+        $user1->setId(1);
+        $user1->setEmail('kgblol059@gmail.com');
+        $user1->setIsActive(true);
+
+        $encoder = $factory->getEncoder($user1);
+        $password = 'test1234';
+        $encoded = $encoder->encodePassword($user1->getPassword(), $password);
+
+        $user1->setPassword($encoded);
+        $user1->setUsername('admin');
+
+        $manager->persist($user1);
+        $manager->flush();
+    }
+
+    public function getOrder()
+    {
+        return 1;
+    }
+}
